57d923af2d7674062410eca28ea171cb78d2ce8e
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-08-31 19:18:10 -0700

    fix: remove legacy token validation from asyn handlers

--- src/routes/blogposts.routes.js
+++ src/routes/blogposts.routes.js
@@ -1,14 +1,13 @@
 const Router = require('@koa/router');
 const validators = require('./validation/blogposts.validation');
-const { validateRequestToken } = require('./validation/tokens.validation');
 const handlers = require('./handlers/blogposts.handlers');
 
 const router = new Router();
 
-router.post('/blog-posts', validateRequestToken, validators.validateCreateRequest, handlers.handleCreateNewBlogPost);
+router.post('/blog-posts', validators.validateCreateRequest, handlers.handleCreateNewBlogPost);
 router.get('/blog-posts', handlers.handleGetManyBlogPosts);
 router.get('/blog-posts/:id', handlers.handleGetSingleBlogPost);
-router.patch('/blog-posts/:id', validateRequestToken, validators.validateUpdateRequest, handlers.handleUpdateBlogPost);
-router.delete('/blog-posts/:id', validateRequestToken, handlers.handleDeleteBlogPost);
+router.patch('/blog-posts/:id', validators.validateUpdateRequest, handlers.handleUpdateBlogPost);
+router.delete('/blog-posts/:id', handlers.handleDeleteBlogPost);
 
 module.exports = router.routes();
